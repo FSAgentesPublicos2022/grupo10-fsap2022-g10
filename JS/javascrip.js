6b82af13503a0f2f20f941110bb0fa759a84dcff
80charlys <carlosafigueroa1980@gmail.com>
2022-06-09 20:13:08 -0300

    subo archivos nuevos

---
+++ JS/javascrip.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+  
+    var htmlObj = document.querySelector('html');
+    var bodyObj = document.querySelector('body');
+  
+    htmlObj.classList.add('h-100');
+  
+    bodyObj.classList.add('d-flex');
+    bodyObj.classList.add('flex-column');
+    bodyObj.classList.add('h-100');
+    
+  });
+  
